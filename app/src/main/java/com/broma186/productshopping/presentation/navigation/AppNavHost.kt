a71cb838521a8615639243ecd9faa4102eecf917
Matthew Brooker <mattbr@hotmail.co.nz>
2025-07-09 12:53:53 +1200

    deeplinking

--- app/src/main/java/com/broma186/productshopping/presentation/navigation/AppNavHost.kt
+++ app/src/main/java/com/broma186/productshopping/presentation/navigation/AppNavHost.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import com.broma186.productshopping.presentation.screens.ProductDetailsScreen
 import com.broma186.productshopping.presentation.screens.ProductsScreen
 import com.broma186.productshopping.presentation.screens.ShoppingCartScreen
@@ -13,17 +14,29 @@ import com.broma186.productshopping.presentation.screens.ShoppingCartScreen
 @Composable
 fun AppNavHost(navController: NavHostController) {
     NavHost(navController, startDestination = Screen.Products.route) {
-        composable(Screen.Products.route) {
+        composable(Screen.Products.route,
+            deepLinks = listOf(navDeepLink {
+                uriPattern = "app://productshopping/productsHome"
+            })
+        ) {
             ProductsScreen(navController)
         }
         composable(
             route = Screen.ProductDetail.route,
-            arguments = listOf(navArgument("productId") { type = NavType.IntType })
+            arguments = listOf(
+                navArgument("productId") { type = NavType.IntType }),
+            deepLinks = listOf(navDeepLink {
+                uriPattern = "app://productshopping/productDetail/{productId}"
+            })
         ) { backStackEntry ->
             val productId = backStackEntry.arguments?.getInt("productId") ?: 0
             ProductDetailsScreen(navController, productId, navController::popBackStack)
         }
-        composable(Screen.ShoppingCart.route) {
+        composable(Screen.ShoppingCart.route,
+            deepLinks = listOf(navDeepLink {
+                uriPattern = "app://productshopping/shoppingCart"
+            })
+        ) {
             ShoppingCartScreen(navController::popBackStack)
         }
     }
